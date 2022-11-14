3b7d65f0470a57b3aefca67f520db3e3b9dd44c6
RetroAkabane <57164699+RetroAkabane@users.noreply.github.com>
2022-11-14 15:58:14 -0500

    Finished all icals and gcals

---
+++ 206.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from bs4 import BeautifulSoup
+import unicodedata
+import requests
+import re
+import lib.database as db
+from lib.reconst import MONTH_REGEX
+import datetime
+from datetime import date
+import lib.ical_scraper as icalscr
+
+
+# In[2]:
+
+
+url = 'https://calendar.google.com/calendar/ical/calendarcb6@gmail.com/public/basic.ics'
+events_dict = icalscr.ical_scraper(url)
+
+
+# In[3]:
+
+
+events_dict
+
+
+# In[3]:
+
+
+database = db.Database(106)
+
+
+# In[4]:
+
+
+database.deleteDistrictEvents()
+database.addDictionary(events_dict)
+
+
+# In[ ]:
+
+
+
+
